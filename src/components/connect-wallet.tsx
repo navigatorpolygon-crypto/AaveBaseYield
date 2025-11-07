7f84b8291faeddb8eeac786d1cf1c752025bbd9f
Paycrypt <navigatorpolygon@gmail.com>
2025-11-07 13:02:17 +0000

    hey fix the wallet connect button to use reown appkit to better UX

--- src/components/connect-wallet.tsx
+++ src/components/connect-wallet.tsx
@@ -1,47 +1,6 @@
 "use client";
 
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { Button } from "./ui/button";
-import { formatAddress } from "@/lib/utils";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-  DropdownMenuSeparator
-} from "./ui/dropdown-menu";
-import { ChevronDown, LogOut, Wallet } from "lucide-react";
-import { Avatar, AvatarFallback } from "./ui/avatar";
-
 export function ConnectWallet() {
-  const { address, isConnected, connector } = useAccount();
-  const { connectors, connect } = useConnect();
-  const { disconnect } = useDisconnect();
-
-  if (isConnected && address) {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" className="flex items-center gap-2">
-             <Avatar className="h-6 w-6">
-                <AvatarFallback className="text-xs bg-primary/10 text-primary">
-                    {address.slice(2, 4).toUpperCase()}
-                </AvatarFallback>
-             </Avatar>
-            <span className="font-mono">{formatAddress(address)}</span>
-            <ChevronDown className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => disconnect()}>
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Disconnect</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  }
-
   // @ts-ignore
   return <appkit-button />;
 }
